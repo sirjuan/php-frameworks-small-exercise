a95bc106999cbc6357fd78e160e84c30ae5ce934
Juha Pekkarinen <sirjuha@gmail.com>
2018-01-17 11:38:28 +0200

    add Message model and refactor AppController to use it

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -4,22 +4,33 @@ namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use App\Entity\Message;
 
 class AppController extends Controller
 {
+    const MESSAGE = 'Hello World!';
+    const CLICKED_MESSAGE = 'Updated Hello World!';
 
     public function index(Request $request)
     {
+        $message = new Message(self::MESSAGE);
+
         $action = $request->query->get('action');
 
+        if ($action == 'click')
+        {
+          $message->setMessage(self::CLICKED_MESSAGE);
+        }
+
         return $this->render('app/app.html.twig', [
-          'text' => $action == 'click' ? 'Updated Hello World!' : 'Hello World!'
+          'text' => $message->getMessage()
         ]);
 
     }
 
     public function handleClick()
     {
+
       return $this->redirectToRoute('app', array('action'=> 'click'));
     }
 
---
+++ src/Entity/Message.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Entity;
+
+class Message
+{
+
+    private $message;
+
+    public function __construct(string $message)
+    {
+        $this->message = $message;
+    }
+
+    public function getMessage(): string
+    {
+        return $this->message;
+    }
+
+    public function setMessage(string $message): void
+    {
+        $this->message = $message;
+    }
+
+}
